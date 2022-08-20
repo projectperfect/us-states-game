87cfefbb7a123b9e3171249202a5aa6ef8222ec9
projectperfect <jprfct@protonmail.com>
2022-08-20 17:49:50 -0500

    game completed

--- main.py
+++ main.py
@@ -16,7 +16,14 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", 
                                     prompt="What's another state's name?").title()
 
-
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("state_to_learn.csv")
+        break
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -26,5 +33,3 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
 
-
-screen.exitonclick()
