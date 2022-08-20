d89c6d5766eaf7d5f75f818b320eeed2bb069151
projectperfect <jprfct@protonmail.com>
2022-08-20 17:07:48 -0500

    imported game map

---
+++ main.py
@@ -0,0 +1,17 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Games")
+
+image = "blank_states_img.gif"
+
+screen.addshape(image)
+
+turtle.shape(image)
+
+
+
+
+
+
+screen.exitonclick()
