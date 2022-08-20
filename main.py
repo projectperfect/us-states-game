bc76f6e3d6bc04b486a89d61f09bb5813fb7a48e
projectperfect <jprfct@protonmail.com>
2022-08-20 17:42:55 -0500

    game functionality completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Games")
@@ -6,12 +7,24 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", 
+                                    prompt="What's another state's name?").title()
 
 
-def get_mouse_click_coor(x, y):
-    print(x, y)
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 
-turtle.onscreenclick(get_mouse_click_coor)
-turtle.mainloop()
 
+screen.exitonclick()
