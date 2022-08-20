76927c24a49d37a2d1326e79d2015b80156c9ea4
projectperfect <jprfct@protonmail.com>
2022-08-20 17:15:20 -0500

    popup that ask the user for a different state

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import turtle
 
 screen = turtle.Screen()
 screen.title("U.S. States Games")
-
 image = "blank_states_img.gif"
-
 screen.addshape(image)
-
 turtle.shape(image)
 
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
 
 
+def get_mouse_click_coor(x, y):
+    print(x, y)
 
+turtle.onscreenclick(get_mouse_click_coor)
+turtle.mainloop()
 
-
-screen.exitonclick()
